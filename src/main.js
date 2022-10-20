e2d10db8f17658555234729b925c9685968ee147
machavarian10to <otar.matchavariani.1@btu.edu.ge>
2022-10-20 12:30:59 +0400

    init commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import router from "./router";
+// import { createPinia } from 'pinia'
+
+import "./index.css";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(router);
+// app.use(createPinia())
+
+app.mount("#app");
---
+++ src/pages/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-5xl text-green-600 m-20">This is a Home!</h1>
+</template>
